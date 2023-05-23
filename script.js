7438ffa6a299f80aced7b5b17f3608220630f8b5
Kaike Yudi <kaikeyudi@outlook.com>
2023-05-23 16:04:48 -0300

    feat: implementa visualização de sumário das informações

--- script.js
+++ script.js
@@ -24,10 +24,59 @@ function count() {
   });
 }
 
+function getSubjects() {
+  const subjects = document.querySelectorAll('input[class="subject"]:checked');
+  let string = '';
+  for (let index = 0; index < subjects.length; index += 1) {
+    if (index === subjects.length - 1) string += `${subjects[index].value}`;
+    else string += `${subjects[index].value}, `;
+  }
+  return string;
+}
+
+function getSummary() {
+  const [{ value: firstname }, { value: lastname }] = document
+    .querySelector('#field-name').children;
+  const email = document.querySelector('#input-email').value;
+  const house = document.querySelector('#house').value;
+  const family = document.querySelector('input[name="family"]:checked').value;
+  const subjects = getSubjects();
+  const rate = document.querySelector('input[name="rate"]:checked').value;
+  const comment = document.querySelector('#textarea').value;
+  return {
+    name: `${firstname} ${lastname}`,
+    email,
+    house,
+    family,
+    subjects,
+    rate,
+    comment,
+  };
+}
+
+function showSummary() {
+  const submit = document.querySelector('#submit-btn');
+  const form = document.querySelector('#evaluation-form');
+  submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    const summary = getSummary();
+    form.innerHTML = `
+      <div>Nome: ${summary.name}<div>
+      <div>Email: ${summary.email}<div>
+      <div>Casa: ${summary.house}<div>
+      <div>Família: ${summary.family}<div>
+      <div>Matérias: ${summary.subjects}<div>
+      <div>Avaliação: ${summary.rate}<div>
+      <div>Observações: ${summary.comment}<div>
+    `;
+  });
+}
+
 function handler() {
   login();
   agreement();
   count();
+  showSummary();
 }
 
 window.onload = handler;
